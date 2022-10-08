f39ad473c9af8774b0e85436354ee04b70002aa4
username <email>
2022-10-08 16:58:11 +0800

    Changes

--- model_build/data_anlysis.py
+++ model_build/data_anlysis.py
@@ -1,7 +1,5 @@
 # !/usr/bin/env python
 # coding=utf-8
-import string
-
 # @Time    : 2022/10/8 15:28
 # @Author  : pineapple
 # @File    : data_analysis
@@ -36,7 +34,7 @@ def calculate_overdue_rate_by_month(
 
 
 def calculate_missing_rate(
-        data: DataFrame, y: str, features: list = None, threshold: float = 0.95) -> object:
+        data: DataFrame, y: str, features: list = None, threshold: float = 0.95) -> dict:
     """
     计算特征的缺失率
     :param data: 数据
@@ -58,10 +56,41 @@ def calculate_missing_rate(
         .reset_index(drop=True)
     )
     del_features_by_missing = missing_data.loc[
-        missing_data["missing_rate"] >= threshold, "feature"
+        missing_data["missing_rate"] >= threshold, "variable"
     ].tolist()
     return {
         "missing_data": missing_data,
         "del_features_by_missing": del_features_by_missing,
     }
 
+
+def calculate_mode_rate(data: DataFrame, y: str,
+                        features: list = None, threshold: float = 0.95) -> dict:
+
+    if features is None:
+        features = data.drop(y, axis=1).columns.tolist()
+    mode_rate_list = []
+    for var in features:
+        mode_rate = data[var].value_counts().values[0] / data.shape[0]
+        mode_rate_list.append(mode_rate)
+    mode_data = (
+        pd.DataFrame({"variable": features, "mode_rate": mode_rate_list})
+        .sort_values(by="mode_rate", ascending=False)
+        .reset_index(drop=True)
+    )
+    del_features_by_mode = mode_data.loc[
+        mode_data["mode_rate"] >= threshold, "variable"
+    ].tolist()
+    return {
+        "mode_data": mode_data,
+        "del_features_by_mode": del_features_by_mode}
+
+
+if __name__ == '__main__':
+    data = pd.DataFrame({
+        "x1": list(np.random.randint(1, 100, 1000)) + [np.nan] * 1000,
+        "y": np.random.randint(0, 2, 2000)
+    })
+
+    missing_data, del_var_by_missing_rate = calculate_missing_rate(data, "y").values()
+    mode_data, del_var_by_mode_rate = calculate_mode_rate(data, "y").values()
